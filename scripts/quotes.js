1afea07a6d82c1325eb1ba752d2df480d6a93708
Jack W. <git@jack.cab>
2024-06-12 19:40:52 -0400

    refactor: Refactor quotes

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,4 +1,4 @@
-quotedb = [
+const quotes = [
   "I'm Tim Sweeney, I'm the founder of Epic Games",
   "you gotta be as gay as possible on the computer otherwise alan turing died for nothing",
   "it's bare clients officThere are no bare clients",
@@ -18,11 +18,11 @@ quotedb = [
   "I got food poisoned,,, That made me angry",
   "https://files.thememesniper.dev/sweeney.zip",
   "why the slop is a homebrew BEAST",
-];
-var chosen = quotedb[Math.floor(Math.random() * quotedb.length)];
-document.getElementById("quote").textContent = `"${chosen}"`;
+]
 
 function quoteReroll() {
-  var chosen = quotedb[Math.floor(Math.random() * quotedb.length)];
-  document.getElementById("quote").textContent = `"${chosen}"`;
+  const chosen = quotes[Math.floor(Math.random() * quotes.length)]
+  document.getElementById("quote").textContent = `"${chosen}"`
 }
+
+quoteReroll()
