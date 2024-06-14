1626d6863176fe5e4c0ec5aeda77e60895375126
Kaitlin <kaitlin@thememesniper.dev>
2024-06-14 04:05:54 -0500

    more 404 quotes

--- src/404quotes.js
+++ src/404quotes.js
@@ -3,6 +3,24 @@ const quotes = [
     "honk shoo honk shoo",
     "Under where?",
     "They're going to SAW OFF, MY FINGERS, I need to build my website.",
+    "Your webpage ran into a problem and needs to renavigate. Stop code: 404",
+    "shiku shiku",
+    "I can't [build] a [website]",
+    "I was never 200 smart I'm 404 smart",
+    "Processor: ARM11 (core 1) Exception type: 404",
+    "Failed to open SafeB9SInstaller.bin",
+    "What is wrong with (my website)",
+    "A visitor? Hmmm... indeed, I have slept long enough.",
+    "That's it. I've reached my breaking point. I'm switching to [React] and...",
+    "Make me a website a shop don't name it weird shit name it like k8's corner or sum...",
+    "Whatttt [astro] is probably too confusing nobody even knows how to code in it...",
+    "Unable to decrypt: The sender's device has not sent us the keys for this webpage.",
+    "coca-cola. COCA-COLA, don't forget de ice. aaaAAAAND: i'm not making money...",
+    "machine, turn back now. the layers of this website are not for your kind.",
+    `The phrase "It's just a [website]" is such a weak mindset. You are ok with what happened...`,
+    "Series of tubes",
+    "Okay, if I, if I chop you up in a meat grinder...",
+    "When I find my site in tons of trouble,\nFriends and colleagues come to me,\nSpeaking words of wisdom,\nwrite in astro"
   ];
   
   const chosen = quotes[Math.floor(Math.random() * quotes.length)];
