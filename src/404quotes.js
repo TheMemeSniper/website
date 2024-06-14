875ed5608e0e4f896f5f1b534bd0a72c36b1ca5a
Kaitlin <kaitlin@thememesniper.dev>
2024-06-14 03:41:46 -0500

    404

---
+++ src/404quotes.js
@@ -0,0 +1,10 @@
+const quotes = [
+    "Gordon, you LOUSY motherfucker,,",
+    "honk shoo honk shoo",
+    "Under where?",
+    "They're going to SAW OFF, MY FINGERS, I need to build my website.",
+  ];
+  
+  const chosen = quotes[Math.floor(Math.random() * quotes.length)];
+  document.getElementById("404quote").textContent = `"${chosen}"`;
+  
