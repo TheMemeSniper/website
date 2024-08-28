a25be7698d4584add307ba9a0c70de033ba3e0f6
Kaitlin <kaitlin@thememesniper.dev>
2024-08-27 21:37:16 -0500

    make it an actual link if it's a link, also Destroy the console.log

--- src/pfp.js
+++ src/pfp.js
@@ -3,17 +3,23 @@ const wetdryprofile = await fetch(
 ).then((res) => res.json())
 const avatar_static = wetdryprofile.avatar_static
 var avatar_source = wetdryprofile.fields[4].value
+const sourceTextObj = document.getElementById("pfpsource")
 
 document.getElementById("pfp").src = avatar_static
 if (avatar_source.startsWith("<a")) {
 	var index = avatar_source.search('href="')
 	if (index == -1) {
-		document.getElementById("pfpsource").textContent = "error while fetching mastodon profile information"
+		sourceTextObj.textContent = "error while fetching mastodon profile information"
 		throw new Error(".search() failed on avatar_source")
 	}
-	console.log(avatar_source.indexOf('"', index + 2))
 	avatar_source = avatar_source.substring(index + 6, avatar_source.indexOf('"', index + 6));
-	
 
-	}
-document.getElementById("pfpsource").textContent = "pfp source: " + avatar_source
+	var a = document.createElement('a');
+	a.innerText = avatar_source
+	a.href = avatar_source
+	sourceTextObj.textContent = "pfp source: " 
+	sourceTextObj.appendChild(a)
+
+} else {
+	sourceTextObj.textContent = "pfp source: " + avatar_source
+}
