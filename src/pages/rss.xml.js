db2b9b2aba74c942eb169d016d32d36bbb09017e
Kaitlin <67394719+TheMemeSniper@users.noreply.github.com>
2026-03-21 19:24:21 -0500

    fix: add missing information to the rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,8 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 
+const icon = "icon.png"
+
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
@@ -10,6 +12,18 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data.metadata,
 			link: `/blog/${post.id}/`,
+			pubDate: post.data.metadata.date,
+			categories: post.data.metadata.tags,
+			author: post.data.metadata.authors,
 		})),
+		/*
+			I cannot even begin to explain what is going on here
+			I am just copying what KDE's RSS feed does
+		*/
+		customData: `
+			<icon>${context.site + icon}</icon>
+			<logo>${context.site + icon}</logo>
+		`,
 	});
 }
+
