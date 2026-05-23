bbfe6a65ad0af989378b9085722a5cc14e12070d
Kaitlin <67394719+TheMemeSniper@users.noreply.github.com>
2026-05-23 08:57:56 -0500

    chore: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig, fontProviders } from "astro/config"
 
-import mdx from "@astrojs/mdx"
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://thememesniper.dev",
-	integrations: [mdx()],
 	fonts: [
 		{
 			provider: fontProviders.fontsource(),
 			name: "JetBrains Mono",
 			cssVariable: "--font-mono",
 		},
-	],
+	]
 })
